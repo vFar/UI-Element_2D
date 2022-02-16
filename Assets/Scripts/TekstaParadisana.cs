a5002ec4a8313316eb8745fa069e0cb24c57489e
Balodis <MarkussDove@gmail.com>
2022-02-16 14:15:02 +0200

    Pievienota teksta nolasīšana ar skriptu

---
+++ Assets/Scripts/TekstaParadisana.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TekstaParadisana : MonoBehaviour {
+	//Uzglabás tekstu, kas panem no text field
+	public string text;
+
+	//Lauks no ka panemsu
+	public GameObject inputField;
+	//Objekts, kurs attelos inputField saturu
+	public GameObject outputText;
+
+	public void TextActions(){
+		text = inputField.GetComponent<Text>().text;
+		outputText.GetComponent<Text> ().text = "Sveiks, " + text.ToUpper()+" !";
+	}
+
+
+
+}
